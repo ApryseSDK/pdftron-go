a7131f96a59d8481e13615a3b3a958ad71d07288
rachmatgithub <cilong@gmail.com>
2021-07-15 23:30:08 +0700

    Updated scripts

--- run-linux.sh
+++ run-linux.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 cd $HOME/go
-go get github.com/PDFTron/pdftron-go
 mv src/github.com/PDFTron/pdftron-go/src_linux/pdftron/ src/
--- run-win.bat
+++ run-win.bat
@@ -1,3 +1,3 @@
 cd %HOMEPATH%
-go get github.com/PDFTron/pdftron-go
+
 xcopy /E /I go\src\github.com\PDFTron\pdftron-go\src_win go\src
