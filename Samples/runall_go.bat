5c9d6fb6c3f832e43a725403d171048bc5dce679
rachmatgithub <cilong@gmail.com>
2021-07-22 14:05:58 +0700

    Update runall_go.bat

--- Samples/runall_go.bat
+++ Samples/runall_go.bat
@@ -5,6 +5,11 @@ if not exist .\bin\ (
 if not exist .\bin\PDFNetC.dll (
 	copy ..\PDFNetC\Lib\PDFNetC.dll .\bin\PDFNetC.dll >nul
 )
+
+if not exist .\bin\pdftron.dll (
+	copy ..\PDFNetC\Lib\pdftron.dll .\bin\pdftron.dll >nul
+)
+
 for /D %%s in (*) do (
     if exist %%s\GO\RunTest.bat (
         cd %%s\GO
