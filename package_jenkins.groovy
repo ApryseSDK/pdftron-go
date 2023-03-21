4293196321fb0dd26c81c8e1c6bc5651d21332ad
Ramzi <rchennafi@pdftron.com>
2023-03-21 16:02:59 -0700

    Added build scripts.

---
+++ package_jenkins.groovy
@@ -0,0 +1,54 @@
+pipeline {
+    agent {
+        docker {
+            label 'linux_fleet'
+            image 'linux_gowrappers:latest'
+            registryUrl 'https://448036597521.dkr.ecr.us-east-1.amazonaws.com'
+            registryCredentialsId 'ecr:us-east-1:Jenkins'
+            alwaysPull true
+        }
+    }
+
+    parameters {
+        string(defaultValue: '', description: 'What name to give the created branch.', name: 'RELEASE_NAME')
+        boolean(defaultValue: false, description: 'Set to true if you wish to push the build to git.', name: 'SHOULD_PUSH')
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                s3ArtifactCopyInvoke('PDFNetWrappers Go/' + params.VERSION, 'PDFTronGoLinux.zip')
+                s3ArtifactCopyInvoke('PDFNetWrappers Go Windows/' + params.VERSION, 'PDFTronGoWindows.zip')
+                s3ArtifactCopyInvoke('PDFNetWrappers Go Mac/' + params.VERSION, 'PDFTronGoMac.zip')
+                s3ArtifactCopyInvoke('PDFNetWrappers Mac Arm/' + params.VERSION, 'PDFTronGoMacArm.zip')
+
+                unzip zipFile: 'PDFTronGoLinux.zip'
+                unzip zipFile: 'PDFTronGoWindows.zip'
+                unzip zipFile: 'PDFTronGoMac.zip'
+                unzip zipFile: 'PDFTronGoMacArm.zip'
+
+                zip zipFile: 'PDFTronGo.zip' dir: '*'
+
+                s3ArtifactUpload('PDFTronGo.zip')
+            }
+        }
+
+        stage('Push') {
+            when { equals: true, actual: params.SHOULD_PUSH}
+            steps {
+                sh """
+                    python3 push-release.py --release-name=${params.RELEASE_NAME}
+                """
+            }
+        }
+    }
+
+    post {
+        failure {
+            sendMail([
+                currentBuild: currentBuild,
+                env: env
+            ])
+        }
+    }
+}
